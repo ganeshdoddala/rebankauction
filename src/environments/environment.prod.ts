3224d0100598b25ef460e73a5cd79a256db76aa0
Ganesh <ganesh.d@makonissoft.com>
2025-09-03 01:01:48 +0530

    github config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: 'https://rebankautionapi.onrender.com/',
+  encryptionKey: '12345678901234567890123456789012', 
+  encryptionIv: '1234567890123456',
+  stateanddistrictsapiKkey:'579b464db66ec23bdd000001bcd9868f1cf141cd785c418d9b50f059'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
   API_URL: 'http://127.0.0.1:5000/', //devapi
-  // API_URL: 'https://rebankautionapi.onrender.com/',
   encryptionKey: '12345678901234567890123456789012', 
   encryptionIv: '1234567890123456',
   stateanddistrictsapiKkey:'579b464db66ec23bdd000001bcd9868f1cf141cd785c418d9b50f059'
